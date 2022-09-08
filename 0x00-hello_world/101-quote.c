7da2fd5d05711e100a23a7fdb8b7f9867b4666f7
Adewunmi Oladele <87828937+Rachamv@users.noreply.github.com>
2022-09-08 18:46:43 -0400

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,27 +1,14 @@
 #include <stdio.h>
 
 /**
- *
- * *main - Entry point
- *
- * *
- *
- * *Return: Always 0 (success)
- *
- * */
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
 
-
-
-
-
-int main( )
+int main(void)
 
 {
-
-	   write( 1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
-
-
-
-	      return 1;
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015 10-19\n", 61;
+	return (1);
 }
+
