165751be980d4f4f8abb5ea5b3171d81e67f2efa
Adewunmi Oladele <rkutalian@gmail.com>
2022-09-08 10:23:58 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * * main - Starting point
+ * *
+ * *
+ * * return: Always 0 (success)
+ * **/
+
+ int main(void)
+ {
+   	 puts("\"Programming is like building a multilingual puzzle");
+	    return (0);
+ }
+
+
