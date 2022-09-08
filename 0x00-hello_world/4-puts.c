91edcf2af3c9fb9bf367c89d61da2decbb50106a
Adewunmi Oladele <87828937+Rachamv@users.noreply.github.com>
2022-09-08 13:43:51 -0400

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,17 @@
 /**
  * * main - Starting point
  * *
- * *
+ * * main: the puts(Programming is like building a multilingual puzzle)
  * * return: Always 0 (success)
  * **/
 
-  int main(void)
-   {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-   }
+int main(void)
+{
+
+   puts("\"Programming is like building a multilingual puzzle");
+
+   return (0);
+
+}
 
 
