3a47e2fe919ec36e0d87334933ad5c744be67bf9
Adewunmi Oladele <rkutalian@gmail.com>
2022-09-08 15:57:26 -0700

    check

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 int main(void)
 
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015 10-19\n", 61;
+	printf("and that piece of art is useful\" - Dora Korpar, 2015 10-19\n", 6);
 	return (1);
 }
 
