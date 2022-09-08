d1ae040ad0348e741211b945101e5d6d4f3222dd
Adewunmi Oladele <rkutalian@gmail.com>
2022-09-08 15:41:49 -0700

    hello

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *
+ * *main - Entry point
+ *
+ * *
+ *
+ * *Return: Always 0 (success)
+ *
+ * */
+
+
+
+
+
+int main( )
+
+{
+
+	   write( 1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+
+
+
+	      return 1;
+
+}
