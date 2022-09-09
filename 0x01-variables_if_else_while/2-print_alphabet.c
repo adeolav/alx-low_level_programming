750199ecb93694704b1593849fe838265aa32a27
Adewunmi Oladele <87828937+Rachamv@users.noreply.github.com>
2022-09-09 16:22:39 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,13 +9,12 @@
  */
 int main(void)
 {
-  int alphabet;
-  
-  for (alphabet ='a'; alphabet <='z'; alphabet++)
-  {
-    alphabet = tolower(alphabet);
-    putchar(alphabet);
-  }
-  putchar('\n');
-  return (0);
+int alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+alphabet = tolower(alphabet);
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
 }
