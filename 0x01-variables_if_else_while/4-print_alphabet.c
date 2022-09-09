0af66bff4901fb2c0902e14d5e253e93c32db36f
Adewunmi Oladele <87828937+Rachamv@users.noreply.github.com>
2022-09-09 22:58:22 +0100

    new file

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include <ctype.h>
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * positive,negative or zero
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'q' || letter == 'e')
+		{
+			continue;
+		}
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
