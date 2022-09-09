bdb936b9d5e8191ea8af473da707ce6acf5daa97
Adewunmi Oladele <rkutalian@gmail.com>
2022-09-08 17:36:03 -0700

    101 quote new

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main-Entry point
  *
@@ -7,7 +8,7 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61;
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 
