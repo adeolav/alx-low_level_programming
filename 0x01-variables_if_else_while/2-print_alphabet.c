b97187813da48fcfa1d69a59d30d2ffd296c7396
Adewunmi Oladele <87828937+Rachamv@users.noreply.github.com>
2022-09-09 16:18:15 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,7 +4,7 @@
 /**
  * main - main block
  * Description: Get a random number and print the number
- * positive,nagetive or zero
+ * positive,negative or zero
  * Return: Always 0 (success)
  */
 int main(void)
