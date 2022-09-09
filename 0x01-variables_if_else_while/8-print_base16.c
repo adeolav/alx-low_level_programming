f721a3241df4af5316e9e359ff1d32487f12d9a5
Adewunmi Oladele <87828937+Rachamv@users.noreply.github.com>
2022-09-09 21:35:03 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,3 +8,22 @@
  * Return: Always 0 (success)
  */
 int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+		if (digit == '9')
+		{
+			digit = 'a';
+			for (; digit <= 'f'; digit++)
+			{
+				putchar(digit);
+			}
+			break;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
