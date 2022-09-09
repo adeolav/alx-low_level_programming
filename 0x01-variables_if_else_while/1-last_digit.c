b9f48e7dd2de5cddb7b92b760373d26acd0abda8
Adewunmi Oladele <87828937+Rachamv@users.noreply.github.com>
2022-09-09 15:28:31 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,8 @@ int main(void)
 		printf("Last digit of %i is %i and is greater than 5\n", n, last);
 	else if (last < 6)
 		printf("Last digit pf %i is %i and is less than 6 and not 0\n", n, last);
-	else
+	{	
 		printf("Last digit of %i is %i and is 0\n", n, last);
+	}
 	return (0);
 }
