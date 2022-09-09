cd94c0254f581868033ade747b62dcb40608fc4c
Adewunmi Oladele <87828937+Rachamv@users.noreply.github.com>
2022-09-09 21:36:17 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,3 +8,18 @@
  * Return: Always 0 (success)
  */
 int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+		if (digit == '9')
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
