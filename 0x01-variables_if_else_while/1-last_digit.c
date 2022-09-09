2855286c17542b2af17e59e08677fc9d6b569252
Adewunmi Oladele <87828937+Rachamv@users.noreply.github.com>
2022-09-09 15:46:13 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -30,7 +30,7 @@ int main(void)
 		}
 		else if (last < 6)
 		{
-			printf("Last digit pf %d is %d and is less than 6 and not 0\n", n, last);
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 		}
 	}
 	return (0);
