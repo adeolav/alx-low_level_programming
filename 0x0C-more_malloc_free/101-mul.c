7936e6f5a55d921ec78099695ff8b185cd157030
Adewunmi Oladele <87828937+Rachamv@users.noreply.github.com>
2022-10-06 20:32:42 +0100

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int find_len(char *str);
-char *create_xarray(int size);
+
+
 char *iterate_zeroes(char *str);
 void get_prod(char *prod, char *mult, int digit, int zeroes);
 void add_nums(char *final_prod, char *next_prod, int next_len);
