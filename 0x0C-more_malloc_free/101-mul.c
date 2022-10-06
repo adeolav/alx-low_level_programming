95d77cd337d1d50563ce874574834c248584d7ab
Adewunmi Oladele <87828937+Rachamv@users.noreply.github.com>
2022-10-06 17:52:12 +0100

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,3 +1,8 @@
+/*
+ * File: 101-mul.c
+ * Auth: Brennan D Baraban
+ */
+
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -40,7 +45,7 @@ char *create_xarray(int size)
 
 	array = malloc(sizeof(char) * size);
 
-	if (!array)
+	if (array == NULL)
 		exit(98);
 
 	for (index = 0; index < (size - 1); index++)
